f28294a1b7046d6579825f6faecdf67baa1d1cf7
Jacopo <179255721+jackc1313@users.noreply.github.com>
2025-05-21 12:12:38 +0200

    test

---
+++ script-test.js
@@ -0,0 +1,14 @@
+let variabileNonUsata = 123 //non usata
+
+
+const variable = "ciao";
+
+function add( a,b){
+return a+ b
+}
+
+if( variable == "ciao"  ){ console.log('messaggio') ;}  ;
+      //spazi inutili
+
+  console.log(add(3, 4));
+
